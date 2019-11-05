e093a9d01561c2e1971d72afe53461e04ba1039a
Max Van Aken <mtvanaken@gmail.com>
2019-11-05 18:02:54 +0000

    Validating sedol codes

--- spec/ISedolValidator.test.js
+++ spec/ISedolValidator.test.js
@@ -24,17 +24,24 @@ describe('Validator', () => {
         expect(validator.state.validationDetails).toEqual("Input string was not 7-characters long")
     })
 
-    test('it returns false when input string is less than 7 chars', () => {
+    test('it returns false when input string is more than 7 chars', () => {
         validator = new ISedolValidator(123456789)
         validator.validate()
         expect(validator.state.isValidSedol).toEqual(false)
         expect(validator.state.validationDetails).toEqual("Input string was not 7-characters long")
     })
 
-    test('it a valid Sedor identifier', () => {
+    test('it a valid Sedol identifier', () => {
         validator = new ISedolValidator("9ABCDE1")
         validator.validate()
         expect(validator.state.isValidSedol).toEqual(true)
         expect(validator.state.validationDetails).toEqual(null)
+    })
+
+    test('is not a valid Sedol identifier', () => {
+        validator = new ISedolValidator("9ABCDE2")
+        validator.validate()
+        expect(validator.state.isValidSedol).toEqual(false)
+        expect(validator.state.validationDetails).toEqual("Checksum digit does not agree with the rest of the input")
     })
 })
--- src/ISedolValidator.js
+++ src/ISedolValidator.js
@@ -6,15 +6,33 @@ class ISedolValidator {
             isUserDefined: null, 
             validationDetails: ""
         }
+        this.weightingFactors = [1,3,1,7,3,9,1]
     }
     validate() {
         if(this.state.inputString === null || this.state.inputString === "" || this.state.inputString.length !== 7) {
             this.state.isValidSedol = false
             this.state.validationDetails = "Input string was not 7-characters long"
         } else {
-            
+            let sedolArr = this.state.inputString.split("")
+            let sedolTotal = 0
+            sedolArr.forEach((item, indexNum) => {
+                if(isNaN(Number(item))) {
+                    sedolTotal += ((item.charCodeAt() - 55) * this.weightingFactors[indexNum])
+                } else {
+                    sedolTotal += (Number(item) * this.weightingFactors[indexNum])
+                }
+            })
+            if(((10 - (sedolTotal % 10)) % 10) === 0) {
+                this.state.isValidSedol = true
+                this.state.validationDetails = null
+            } else {
+                this.state.isValidSedol = false
+                this.state.validationDetails = "Checksum digit does not agree with the rest of the input"
+            }
         }
     }
 }
 
 module.exports = {ISedolValidator}
+// console.log("A".charCodeAt() - 55)
+// console.log("B".charCodeAt() - 55)
